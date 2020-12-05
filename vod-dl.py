dd1c99faa79e98dd0d75cd714752a0cf6d85f2a7
Gavin Mason <github@gavinm.us>
2020-12-04 20:17:52 -0500

    Removed external config support and fixed some bugs

--- vod-dl.py
+++ vod-dl.py
@@ -1,5 +1,4 @@
 import os, sys, shutil
-from xml.dom import minidom
 from datetime import date, datetime, timedelta
 
 def display_help(error=''):
@@ -9,8 +8,7 @@ def display_help(error=''):
      vod-dl [OPTIONS]\n
     Options:\n
      -g GAMES         games you want the vods to have (optional, default is any)\n
-     -s STREAMERS     the streamers whose vods you want to download, you can specify
-                      "all" (optional, default is all)\n
+     -s STREAMERS     the streamers whose vods you want to download (mandatory)\n
      -d DATES         the dates to download from, options are: "today", "yesterday",
                       or "month/day/year" (optional, default is today)\n
      -f FOLDER        the folder to download the vods to (optional, default is current dir)\n
@@ -18,22 +16,10 @@ def display_help(error=''):
                       medium, low (optional, default is you have to choose)\n
      -m FILETYPE      the filetype of the vods you want to download (optional,
                       default is mp4)\n
-     -c CONFIG        the xml file(s) that include the streamers whose vods you want to 
-                      download from, it is a more permanent and shortened version of streamers,
-                      to choose from this specify all for streamers (optional, default is config.xml
-                      in current directory)\n
      -h               display this help message and exit\n''')
     sys.exit()
 
-def get_channels(filename):
-    try:
-        file = minidom.parse(os.path.join(os.getcwd(), filename))
-    except IOError:
-        return None
-    channels = file.getElementsByTagName('channel')
-    return channels
-
-def check_args(input, all):
+def check_args(input):
     games = []
     streamers = []
     quality = ''
@@ -45,7 +31,6 @@ def check_args(input, all):
     quality_count = 0
     filetype_count = 0
     date_count = 0
-    all = []
     qualities = {'high':'source', 'medium':'720', 'low':'480'}
     option = 's'
     for arg in input:
@@ -68,8 +53,6 @@ def check_args(input, all):
                     display_help('Entered more than one argument for filetypes! Incorrect usage!')
                 filetype = arg
                 filetype_count += 1
-            elif option == 'c':
-                all.extend(get_streamers(arg))
             elif option == 'd':
                 if date_count >= 1:
                     display_help('Entered more than one argument for date! Incorrect usage!')
@@ -84,10 +67,8 @@ def check_args(input, all):
             display_help()
         elif option not in 'sgqfmd':
             display_help('Entered an incorrect option! Incorrect usage!')
-    if not configs:
-        all = get_streamers('config.xml')
-    if 'all' in streamers or not streamers:
-        streamers = all
+    if not streamers:
+        display_help('You must enter at least one streamer!')
     return streamers, games, folder, day, quality, filetype
 
 def get_raw_vods(streamers, games, folder):
@@ -125,7 +106,7 @@ def download_vods(vods, quality, filetype, folder):
     for streamer, vod in vods.items():
         if quality:
             quality_arg = '-q ' + quality
-        os.system(f'TMP={os.path.join(folder, 'tmp/')} twitch-dl download {quality_arg} -f {filetype} {vod}')
+        os.system(f'TMP={os.path.join(folder, "tmp/")} twitch-dl download {quality_arg} -f {filetype} {vod}')
         print(f'DONE WITH DOWNLOADING {vod}')
 
 def make_folder(folder):
@@ -139,7 +120,7 @@ def erase_folder(folder):
             os.remove(os.path.join(folder, filename))
 
 if __name__ == '__main__':
-    streamers, games, template, day, quality, filetype = check_args(sys.argv[1:], channels)
+    streamers, games, template, day, quality, filetype = check_args(sys.argv[1:])
     folder = os.path.join(template, day.strftime("%y-%m-%d"))
     make_folder(template)
     erase_folder(os.path.join(template, 'tmp/'))
